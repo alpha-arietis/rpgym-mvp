c0f70474c8da784f6b240db5c74d84a6f8e94648
alpha-arietis <58612800+alpha-arietis@users.noreply.github.com>
2020-03-05 17:42:02 +1300

    t

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-// Connect to DB
+// Connect to DB - HEROKU works with hardcoded URI
 const uri = process.env.ATLAS_URI
-mongoose.connect('mongodb://rpgym:rpgym@cluster0-shard-00-00-volnf.gcp.mongodb.net:27017,cluster0-shard-00-01-volnf.gcp.mongodb.net:27017,cluster0-shard-00-02-volnf.gcp.mongodb.net:27017/RPGym?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 const connection = mongoose.connection
 connection.once('open', () => {
     console.log('Connection to MongoDB established successfully')
