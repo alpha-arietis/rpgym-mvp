91712fe9ce36b374147c5138121b49ae32ca1ddf
alpha-arietis <58612800+alpha-arietis@users.noreply.github.com>
2020-03-05 18:00:13 +1300

    t

--- server.js
+++ server.js
@@ -16,13 +16,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-// Connect to DB - HEROKU works with hardcoded URI
-const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-const connection = mongoose.connection
-connection.once('open', () => {
-    console.log('Connection to MongoDB established successfully')
-})
+
 
 // Bodyparser (ensures we can get data from req.body in our html form)
 app.use(express.urlencoded({ extended: false }))
@@ -41,8 +35,16 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(`${__dirname}/frontend/build/index.html`))
 }
 
+// Connect to DB - HEROKU works with hardcoded URI
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('Connection to MongoDB established successfully')
+})
+
 // Start Server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`, process.env.TEST)
+    console.log(`Server is running on port ${port}`, process.env.ATLAS_URI)
 }
 )
