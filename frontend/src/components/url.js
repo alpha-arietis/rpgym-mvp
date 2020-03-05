8d0525ad45064df253c25792d2171dfd9f253dd0
alpha-arietis <58612800+alpha-arietis@users.noreply.github.com>
2020-03-05 14:33:18 +1300

    update env url

--- frontend/src/components/url.js
+++ frontend/src/components/url.js
@@ -1,2 +1,5 @@
-const url = process.env.NODE_ENV==='production' ? `${window.location.href}/` : 'http://localhost:5000/'
+const url = process.env.NODE_ENV==='production' ? 'https://rpgym-mvp.herokuapp.com/' : 'http://localhost:5000/'
 export default url
+
+// 
+//`${window.location.href}/`
